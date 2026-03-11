b41ad55ad70556a2318d0a79f7e5c5d41fee1583
Kalyan9912 <relangimavayya123@gmail.com>
2026-03-11 14:35:14 +0530

    Update TransferForm.js

--- FRONT-END/src/components/TransferForm.js
+++ FRONT-END/src/components/TransferForm.js
@@ -1,1 +1,19 @@
+import React from "react";
 
+function TransferForm() {
+
+  return (
+    <div style={{border:"1px solid #ccc",padding:"20px",margin:"10px"}}>
+
+      <h3>Quick Transfer</h3>
+
+      <input placeholder="Account" /><br/><br/>
+      <input placeholder="Amount" /><br/><br/>
+
+      <button>Send</button>
+
+    </div>
+  );
+}
+
+export default TransferForm;
