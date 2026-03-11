14b40c8c792c5a323dfbed821272622d46aeff36
Kalyan9912 <relangimavayya123@gmail.com>
2026-03-11 14:34:45 +0530

    Update TransactionList.js

--- FRONT-END/src/components/TransactionList.js
+++ FRONT-END/src/components/TransactionList.js
@@ -1,1 +1,23 @@
+import React from "react";
 
+function TransactionList() {
+
+  const transactions = [
+    {name:"Amazon",amount:"-120"},
+    {name:"Salary",amount:"+3000"},
+    {name:"Utility Bill",amount:"-80"}
+  ];
+
+  return (
+    <div style={{border:"1px solid #ccc",padding:"20px",margin:"10px"}}>
+      <h3>Recent Transactions</h3>
+
+      {transactions.map((t,index)=>(
+        <p key={index}>{t.name} : {t.amount}</p>
+      ))}
+
+    </div>
+  );
+}
+
+export default TransactionList;
