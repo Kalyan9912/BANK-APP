245be7485475cf402ec4739206cc3b71f07573ec
Kalyan9912 <relangimavayya123@gmail.com>
2026-03-11 14:34:23 +0530

    Update BalanceCard.js

--- FRONT-END/src/components/BalanceCard.js
+++ FRONT-END/src/components/BalanceCard.js
@@ -1,1 +1,12 @@
+import React from "react";
 
+function BalanceCard() {
+  return (
+    <div style={{border:"1px solid #ccc",padding:"20px",margin:"10px"}}>
+      <h3>Total Balance</h3>
+      <h1>$12,450</h1>
+    </div>
+  );
+}
+
+export default BalanceCard;
