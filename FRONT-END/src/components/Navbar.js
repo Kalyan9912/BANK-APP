8b734b3a1dea9a0d46c83cc68fab6268ef0d1aaf
Kalyan9912 <relangimavayya123@gmail.com>
2026-03-11 14:33:57 +0530

    Update Navbar.js

--- FRONT-END/src/components/Navbar.js
+++ FRONT-END/src/components/Navbar.js
@@ -1,1 +1,11 @@
+import React from "react";
 
+function Navbar() {
+  return (
+    <div style={{background:"#1e88e5",color:"white",padding:"10px"}}>
+      <h2>FrontBank</h2>
+    </div>
+  );
+}
+
+export default Navbar;
