67cf9a51bf220fadb4457211ec219735a98d0ffe
Kalyan9912 <relangimavayya123@gmail.com>
2026-03-11 14:35:54 +0530

    Update Dashboard.js

--- FRONT-END/src/pages/Dashboard.js
+++ FRONT-END/src/pages/Dashboard.js
@@ -1,1 +1,21 @@
+import React from "react";
+import BalanceCard from "../components/BalanceCard";
+import TransactionList from "../components/TransactionList";
+import TransferForm from "../components/TransferForm";
 
+function Dashboard() {
+
+  return (
+    <div style={{padding:"20px"}}>
+
+      <h1>Welcome to FrontBank</h1>
+
+      <BalanceCard />
+      <TransactionList />
+      <TransferForm />
+
+    </div>
+  );
+}
+
+export default Dashboard;
