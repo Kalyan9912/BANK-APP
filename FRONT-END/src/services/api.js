8bd4af5aa74d98bfcd83a7dd8596da4d8095a19c
Kalyan9912 <relangimavayya123@gmail.com>
2026-03-11 14:36:18 +0530

    Update api.js

--- FRONT-END/src/services/api.js
+++ FRONT-END/src/services/api.js
@@ -1,1 +1,11 @@
+import axios from "axios";
 
+const API_URL = "http://localhost:8080/api";
+
+export const getAccounts = () => {
+  return axios.get(`${API_URL}/accounts`);
+};
+
+export const getTransactions = () => {
+  return axios.get(`${API_URL}/transactions`);
+};
