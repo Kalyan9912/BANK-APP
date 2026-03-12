9f6cf7d2e2b43ec0556c5b672fd061910409da46
Kalyan9912 <relangimavayya123@gmail.com>
2026-03-12 15:09:04 +0530

    Update BalanceCard.js

--- FRONT-END/src/components/BalanceCard.js
+++ FRONT-END/src/components/BalanceCard.js
@@ -9,8 +9,19 @@ function BalanceCard(){
       <div className="card-body">
 
         <h5>Total Balance</h5>
-
         <h2>$12,450.00</h2>
+
+        <hr/>
+
+        <div className="d-flex justify-content-between mt-3">
+          <span>Checking</span>
+          <span className="text-success">$12,350.00</span>
+        </div>
+
+        <div className="d-flex justify-content-between">
+          <span>Savings</span>
+          <span className="text-success">$80.00</span>
+        </div>
 
       </div>
 
