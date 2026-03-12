898ecbfed49311996c9ab235a8a8f21b736e293f
Kalyan9912 <relangimavayya123@gmail.com>
2026-03-12 15:55:45 +0530

    Update MonthlyChart.js

--- FRONT-END/src/components/MonthlyChart.js
+++ FRONT-END/src/components/MonthlyChart.js
@@ -1,44 +1,65 @@
 import React from "react";
+import {
+  Chart as ChartJS,
+  CategoryScale,
+  LinearScale,
+  PointElement,
+  LineElement,
+  Title,
+  Tooltip,
+  Legend
+} from "chart.js";
+
 import { Line } from "react-chartjs-2";
 
-function MonthlyChart(){
+ChartJS.register(
+  CategoryScale,
+  LinearScale,
+  PointElement,
+  LineElement,
+  Title,
+  Tooltip,
+  Legend
+);
+
+function MonthlyChart() {
 
   const data = {
-    labels: ["Jan","Feb","Mar","Apr","May","Jun"],
+    labels: ["Jan", "Feb", "Mar", "Apr", "May", "Jun"],
     datasets: [
       {
         label: "Balance",
-        data: [200,350,500,650,720,900],
+        data: [100, 250, 400, 550, 700, 900],
         borderColor: "#3a6fd8",
         backgroundColor: "rgba(58,111,216,0.2)",
-        tension: 0.4
+        tension: 0.4,
+        fill: true
       }
     ]
   };
 
   const options = {
     responsive: true,
-    maintainAspectRatio: false
+    maintainAspectRatio: false,
+    plugins: {
+      legend: {
+        display: false
+      }
+    }
   };
 
-  return(
-
+  return (
     <div className="card shadow">
-
       <div className="card-body">
-
         <h6>Monthly Balance</h6>
 
-        <div style={{height:"200px"}}>
-          <Line data={data} options={options}/>
+        <div style={{ height: "200px" }}>
+          <Line data={data} options={options} />
         </div>
 
       </div>
-
     </div>
-
-  )
-
+  );
 }
 
-export default MonthlyChart;
+export default MonthlyChart;;
