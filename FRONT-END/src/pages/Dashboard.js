326a08c99dc0f3efd137e44f99c4b7cc8fda2181
Kalyan9912 <relangimavayya123@gmail.com>
2026-03-12 15:03:10 +0530

    Update Dashboard.js

--- FRONT-END/src/pages/Dashboard.js
+++ FRONT-END/src/pages/Dashboard.js
@@ -2,32 +2,54 @@ import React from "react";
 import BalanceCard from "../components/BalanceCard";
 import TransactionList from "../components/TransactionList";
 import TransferForm from "../components/TransferForm";
+import Navbar from "../components/Navbar";
 
-function Dashboard() {
+function Dashboard(){
 
-  return (
-    <div className="container mt-4">
+  return(
 
-      <h2 className="mb-4">Welcome to FrontBank</h2>
+    <div>
 
-      <div className="row">
+      <Navbar/>
 
-        <div className="col-md-4">
-          <BalanceCard />
+      <div className="hero-section">
+
+        <div className="container">
+
+          <h2 className="fw-bold">
+            Welcome to <span className="text-primary">FrontBank, John!</span>
+          </h2>
+
+          <p>Your secure banking dashboard.</p>
+
         </div>
 
-        <div className="col-md-4">
-          <TransactionList />
-        </div>
+      </div>
 
-        <div className="col-md-4">
-          <TransferForm />
+      <div className="container mt-4">
+
+        <div className="row g-4">
+
+          <div className="col-md-4">
+            <BalanceCard/>
+          </div>
+
+          <div className="col-md-4">
+            <TransactionList/>
+          </div>
+
+          <div className="col-md-4">
+            <TransferForm/>
+          </div>
+
         </div>
 
       </div>
 
     </div>
-  );
+
+  )
+
 }
 
 export default Dashboard;
