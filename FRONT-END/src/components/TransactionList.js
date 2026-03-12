fb7acfbadb10aba04bdaee4e8d78921c039b5fa0
Kalyan9912 <relangimavayya123@gmail.com>
2026-03-12 14:55:15 +0530

    Update TransactionList.js

--- FRONT-END/src/components/TransactionList.js
+++ FRONT-END/src/components/TransactionList.js
@@ -3,23 +3,35 @@ import React from "react";
 function TransactionList(){
 
   return(
+
     <div className="card shadow">
+
+      <div className="card-header">
+        Recent Transactions
+      </div>
 
       <div className="card-body">
 
-        <h5>Recent Transactions</h5>
-
         <ul className="list-group">
 
-          <li className="list-group-item">Amazon : -120</li>
-          <li className="list-group-item text-success">Salary : +3000</li>
-          <li className="list-group-item">Utility Bill : -80</li>
+          <li className="list-group-item">
+            Amazon : -120
+          </li>
+
+          <li className="list-group-item text-success">
+            Salary : +3000
+          </li>
+
+          <li className="list-group-item">
+            Utility Bill : -80
+          </li>
 
         </ul>
 
       </div>
 
     </div>
+
   )
 
 }
