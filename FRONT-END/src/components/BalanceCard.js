e2551dc67acbc60877f32e3a46c5808bef674645
Kalyan9912 <relangimavayya123@gmail.com>
2026-03-12 14:53:01 +0530

    Update BalanceCard.js

--- FRONT-END/src/components/BalanceCard.js
+++ FRONT-END/src/components/BalanceCard.js
@@ -3,13 +3,13 @@ import React from "react";
 function BalanceCard(){
 
   return(
-    <div className="card shadow">
+    <div className="card balance-card shadow">
 
       <div className="card-body text-center">
 
         <h5>Total Balance</h5>
 
-        <h2 className="text-primary">$12,450</h2>
+        <h2>$12,450</h2>
 
       </div>
 
