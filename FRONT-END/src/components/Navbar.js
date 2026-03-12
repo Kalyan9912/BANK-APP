3152c59a1e23d46df8c709c0e7f7cf5724063bc6
Kalyan9912 <relangimavayya123@gmail.com>
2026-03-12 15:02:40 +0530

    Update Navbar.js

--- FRONT-END/src/components/Navbar.js
+++ FRONT-END/src/components/Navbar.js
@@ -1,11 +1,43 @@
 import React from "react";
 
-function Navbar() {
-  return (
-    <div style={{background:"#1e88e5",color:"white",padding:"10px"}}>
-      <h2>FrontBank</h2>
-    </div>
-  );
+function Navbar(){
+
+  return(
+
+    <nav className="navbar navbar-expand-lg navbar-light bg-white shadow-sm">
+
+      <div className="container">
+
+        <a className="navbar-brand fw-bold text-primary" href="#">
+          FrontBank
+        </a>
+
+        <ul className="navbar-nav ms-auto">
+
+          <li className="nav-item">
+            <a className="nav-link">Home</a>
+          </li>
+
+          <li className="nav-item">
+            <a className="nav-link">Accounts</a>
+          </li>
+
+          <li className="nav-item">
+            <a className="nav-link">Transfer</a>
+          </li>
+
+          <li className="nav-item">
+            <a className="nav-link">Profile</a>
+          </li>
+
+        </ul>
+
+      </div>
+
+    </nav>
+
+  )
+
 }
 
 export default Navbar;
