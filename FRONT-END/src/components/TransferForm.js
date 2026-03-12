8c601b1e750873fec8b8910e4b2b95357d04f0c9
Kalyan9912 <relangimavayya123@gmail.com>
2026-03-12 14:47:25 +0530

    Update TransferForm.js

--- FRONT-END/src/components/TransferForm.js
+++ FRONT-END/src/components/TransferForm.js
@@ -1,20 +1,27 @@
 import React from "react";
 
-function TransferForm() {
-  return (
+function TransferForm(){
+
+  return(
+
     <div className="card shadow">
+
       <div className="card-body">
 
         <h5>Quick Transfer</h5>
 
         <input className="form-control mb-2" placeholder="Account"/>
+
         <input className="form-control mb-2" placeholder="Amount"/>
 
         <button className="btn btn-primary">Send</button>
 
       </div>
+
     </div>
-  );
+
+  )
+
 }
 
 export default TransferForm;
