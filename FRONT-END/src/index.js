9bf2393b027d3a62bcedae68bb7dddba42164841
Kalyan9912 <relangimavayya123@gmail.com>
2026-03-12 12:41:28 +0530

    Update index.js

--- FRONT-END/src/index.js
+++ FRONT-END/src/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
-import "./styles.css";   // 👈 ADD THIS LINE
+import "bootstrap/dist/css/bootstrap.min.css";   // ADD THIS
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
